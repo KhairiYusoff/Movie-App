bcc1c03d0dd22bd11e7e74e1756f6a06e6f2bbc4
KHAIRI <you@example.com>
2022-08-16 20:42:35 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import styled from 'styled-components'
+
+const Container = styled.div`
+text-align: center;
+`
+
+
+const App = ()=>{
+  return(
+    <Container>SAYA</Container>
+    
+  )
+}
+
+export default App;
