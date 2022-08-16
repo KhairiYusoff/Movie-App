95dd871d91edccdd3ea3ef2a3606c1e88b19cd2f
KHAIRI <you@example.com>
2022-08-16 23:05:05 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,33 +1,56 @@
-
-import styled from 'styled-components'
-import React, { useEffect} from 'react'
-import './App.css'
-import searchIcon from './search.svg'
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { FaSearch } from "react-icons/fa";
+import MovieCard from "./MovieCard";
 
 //663984e4
 
-const API_URL = 'http://www.omdbapi.com/?apikey=663984e4'
+const API_URL = "http://www.omdbapi.com/?apikey=663984e4";
 
-const Container = styled.div`
-text-align: center;
-`
+// const movie1 = {
+//   Title: "Pendekar bujang lapok",
+//   Year: "1959",
+//   imdbID: "tt0277950",
+//   Type: "movie",
+//   Poster:
+//     "https://m.media-amazon.com/images/M/MV5BYzZjYjZmMzUtOWUzYy00MDkwLThhMWMtMDdiYjJjNzcyNzRhXkEyXkFqcGdeQXVyMTA3NTk1MQ@@._V1_SX300.jpg",
+// };
+
+const App = () => {
+
+  const [ movies,setMovies] = useState([])
+
+  const [searchterm, setSearchterm] = useState("")
+
+  useEffect(() => {
+    searchMovies("title");
+  }, []);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  };
 
 
-const App = ()=>{
-
-const searchMovies = async (title) => {
-const response = await fetch (`${API_URL}&s=${title}`);
-const data = await response.json();
-console.log(data.Search[0])
-}
-
-  useEffect(()=>{
-    searchMovies('pendekar')
-  },[])
-  return(
-    <Container>SAYA</Container>
-    
-  )
-}
+  return (
+    <div className="app">
+      <h1>Movie Search</h1>
+      <div className="search">
+        <input
+          placeholder="search your movie"
+          value={searchterm}
+          onChange={(e) => {setSearchterm(e.target.value)}}
+        />
+        <FaSearch className="faIcon" onClick={() => {searchMovies(searchterm)}} />
+      </div>
+      <div className="container">
+      {movies.map(movie=>(
+        <MovieCard movie={movie}/>
+      ))}
+      </div>
+    </div>
+  );
+};
 
 export default App;
