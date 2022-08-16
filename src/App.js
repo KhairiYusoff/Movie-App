82c10e0c987befb0530958000446c86718e632e4
KHAIRI <you@example.com>
2022-08-16 21:33:56 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 
 import styled from 'styled-components'
+import React, { useEffect} from 'react'
+import './App.css'
+import searchIcon from './search.svg'
+
+//663984e4
+
+const API_URL = 'http://www.omdbapi.com/?apikey=663984e4'
 
 const Container = styled.div`
 text-align: center;
@@ -7,6 +14,16 @@ text-align: center;
 
 
 const App = ()=>{
+
+const searchMovies = async (title) => {
+const response = await fetch (`${API_URL}&s=${title}`);
+const data = await response.json();
+console.log(data.Search[0])
+}
+
+  useEffect(()=>{
+    searchMovies('pendekar')
+  },[])
   return(
     <Container>SAYA</Container>
     
